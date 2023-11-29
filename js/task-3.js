a076d13ecad4f28d427c44d2ee62e485bf9c9e66
Vitalii Tieliehin <vantaglist@gmail.com>
2023-11-29 21:18:41 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,11 @@
 function checkForSpam(message){
-    if(message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale')){
-        return true;
-    } else {
-        return false;
-    }
+    return message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale');
 }
 
-console.log(checkForSpam("Latest technology news"));                          //повертає false
-console.log(checkForSpam("JavaScript weekly newsletter"));                    //повертає false
-console.log(checkForSpam("Get best sale offers now!"));                       //повертає true
-console.log(checkForSpam("Amazing SalE, only tonight!"));                     //повертає true
-console.log(checkForSpam("Trust me, this is not a spam message"));            //повертає true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));    //повертає true
-console.log(checkForSpam("[SPAM] How to earn fast money?"));                  //повертає true
+console.log(checkForSpam("Latest technology news"));                          //false
+console.log(checkForSpam("JavaScript weekly newsletter"));                    //false
+console.log(checkForSpam("Get best sale offers now!"));                       //true
+console.log(checkForSpam("Amazing SalE, only tonight!"));                     //true
+console.log(checkForSpam("Trust me, this is not a spam message"));            //true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));    //true
+console.log(checkForSpam("[SPAM] How to earn fast money?"));                  //true
